10c6015bfc3c9b78d67d0f75abd594594acc4f1d
Ivan Krasin <imkrasin@gmail.com>
2014-06-24 23:56:22 -0700

    Don't download toolchain on Windows: there's no way to unzip it; make CMake download compatible with make. Now, they extract to the same dir.

--- Tools/ARMToolchain.cmake
+++ Tools/ARMToolchain.cmake
@@ -11,34 +11,30 @@ IF(CMAKE_SYSTEM_NAME STREQUAL "Linux")
   SET(TOOLS_SUFFIX "linux.tar.bz2")
 ELSEIF(CMAKE_SYSTEM_NAME STREQUAL "Darwin")
   SET(TOOLS_SUFFIX "mac.tar.bz2")
-ELSEIF(CMAKE_SYSTEM_NAME STREQUAL "Windows")
-  SET(TOOLS_SUFFIX "win32.zip")
-ELSE()
-  MESSAGE(FATAL_ERROR "Unsupported system: ${CMAKE_SYSTEM_NAME}")
 ENDIF()
 
 SET(TOOLS_ARCHIVE_NAME "gcc-arm-none-eabi-${TOOLS_VERSION_FILENAME}-${TOOLS_SUFFIX}")
 SET(TOOLS_URL "${TOOLS_URL_PREFIX}/${TOOLS_ARCHIVE_NAME}")
 
-IF(NOT IS_DIRECTORY "${CMAKE_SOURCE_DIR}/../Tools/arm-eabi-toolchain")
+IF(TOOLS_SUFFIX AND NOT IS_DIRECTORY "${CMAKE_SOURCE_DIR}/../Tools/gcc-arm-none-eabi")
 message("Downloading ARM EABI toolchain from ${TOOLS_URL} ...")
 file(DOWNLOAD ${TOOLS_URL} "${CMAKE_SOURCE_DIR}/../Tools/${TOOLS_ARCHIVE_NAME}" SHOW_PROGRESS)
 execute_process(COMMAND ${CMAKE_COMMAND} -E tar xhjf "${TOOLS_ARCHIVE_NAME}"
   WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}/../Tools/")
-file(RENAME "${CMAKE_SOURCE_DIR}/../Tools/${TOOLS_EXPANDED_DIRNAME}" "${CMAKE_SOURCE_DIR}/../Tools/arm-eabi-toolchain")
-file(REMOVE "${CMAKE_SOURCE_DIR}/../Tools/{TOOLS_ARCHIVE_NAME}")
+file(RENAME "${CMAKE_SOURCE_DIR}/../Tools/${TOOLS_EXPANDED_DIRNAME}" "${CMAKE_SOURCE_DIR}/../Tools/gcc-arm-none-eabi")
+file(REMOVE "${CMAKE_SOURCE_DIR}/../Tools/${TOOLS_ARCHIVE_NAME}")
 ENDIF()
 
 SET(CMAKE_SYSTEM_NAME Generic)
 SET(CMAKE_SYSTEM_VERSION 1)
-SET(CMAKE_C_COMPILER ${PROJECT_SOURCE_DIR}/../Tools/arm-eabi-toolchain/bin/arm-none-eabi-gcc)
-SET(CMAKE_CXX_COMPILER ${PROJECT_SOURCE_DIR}/../Tools/arm-eabi-toolchain/bin/arm-none-eabi-g++)
-SET(OBJCOPY ${PROJECT_SOURCE_DIR}/../Tools/arm-eabi-toolchain/bin/arm-none-eabi-objcopy)
+SET(CMAKE_C_COMPILER ${PROJECT_SOURCE_DIR}/../Tools/gcc-arm-none-eabi/bin/arm-none-eabi-gcc)
+SET(CMAKE_CXX_COMPILER ${PROJECT_SOURCE_DIR}/../Tools/gcc-arm-none-eabi/bin/arm-none-eabi-g++)
+SET(OBJCOPY ${PROJECT_SOURCE_DIR}/../Tools/gcc-arm-none-eabi/bin/arm-none-eabi-objcopy)
 
 SET(CMAKE_FIND_ROOT_PATH
-  ${PROJECT_SOURCE_DIR}/../Tools/arm-eabi-toolchain/arm-none-eabi/bin
-  ${PROJECT_SOURCE_DIR}/../Tools/arm-eabi-toolchain/arm-none-eabi/lib
-  ${PROJECT_SOURCE_DIR}/../Tools/arm-eabi-toolchain/arm-none-eabi/include
+  ${PROJECT_SOURCE_DIR}/../Tools/gcc-arm-none-eabi/arm-none-eabi/bin
+  ${PROJECT_SOURCE_DIR}/../Tools/gcc-arm-none-eabi/arm-none-eabi/lib
+  ${PROJECT_SOURCE_DIR}/../Tools/gcc-arm-none-eabi/arm-none-eabi/include
  )
 SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM ONLY)
 SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
